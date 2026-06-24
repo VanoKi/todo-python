35b8b1884497cdeffef57916f5f8b888f98d8733
VanoKi <kirsvano@gmail.com>
2026-06-24 21:36:24 +0300

    feat: inheretance

--- lesson_inheretance.py
+++ lesson_inheretance.py
@@ -1,12 +1,16 @@
 class Animal:
-    def __init__(self, name, sound):
+    def __init__(self, name):
         self.name = name
 
     def speak(self):
         return '...'
 
+dog = Animal('Dog')
+
+print(dog.speak())
+
 class Dog(Animal):
     def speak(self):
         return f'{self.name} says Woof'
 
-print(Dog('Dog', 'Woof').speak())
+print(Dog('Dog').speak())
